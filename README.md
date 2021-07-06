3165081bf8b9b129999afe01f4e9a77ba8fe4ff2
Jeffrey Ding <jeffreyd99@hotmail.com>
2021-07-05 21:28:17 -0400

    added files

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple enumerables rebuilt using yield and call methods.
---
+++ enumerables.rb
@@ -0,0 +1,9 @@
+module Enumerable
+    def my_each
+        
+    end
+
+end
+
+puts "my_each vs. each"
+numbers = [1, 2, 3, 4, 5]
